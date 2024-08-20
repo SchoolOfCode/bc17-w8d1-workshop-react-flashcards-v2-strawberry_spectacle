030cf15b6171a6364b7816146f746202b1a88136
Ellie Parry <ellierparry@gmail.com>
2024-08-20 09:45:48 +0100

    Flashcard Qs array put into App function.

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,28 @@ import Flashcards from "./components/Flashcards/Flashcards";
 
 function App() {
   
-    // cons= [flashcardBank, setFlashcardBank] = useState ([
+    cons= [flashcardBank, setFlashcardBank] = useState ([
       
-    //     { question: "What is JSX?", 
-    //       answer: "JSX stands for JavaScript XML. It allows you to write HTML elements in JavaScript and place them in the DOM without using `createElement()` or `appendChild()`." }, 
-    //     { question: "How do you create a React component?", 
-    //       answer: "You can create a React component either as a function or as a class. Function components are simple JavaScript functions, while class components require extending `React.Component`." }, 
-    //     { question: "What is the virtual DOM?", 
-    //       answer: "The virtual DOM is a lightweight copy of the actual DOM. React uses the virtual DOM to determine which elements need to be updated, making updates more efficient." }
-    // ])
+        { question: "What is JSX?", 
+          answer: "JSX stands for JavaScript XML. It allows you to write HTML elements in JavaScript and place them in the DOM without using `createElement()` or `appendChild()`." }, 
+        { question: "How do you create a React component?", 
+          answer: "You can create a React component either as a function or as a class. Function components are simple JavaScript functions, while class components require extending `React.Component`." }, 
+        { question: "What is the virtual DOM?", 
+          answer: "The virtual DOM is a lightweight copy of the actual DOM. React uses the virtual DOM to determine which elements need to be updated, making updates more efficient." }
+    ])
 
     return (
     <>
       <Header />
       <Form />
-      <Flashcards/>
+      {/* <Flashcards/> */}
+
+      <section>
+        { flashcardBank.map((card) => {
+          return <article>{card.question} {card.answer}</article>
+        })}
+      </section>
+
       <Footer/>
     </>
   );
--- src/components/Flashcards/Flashcard/Flashcard.jsx
+++ src/components/Flashcards/Flashcard/Flashcard.jsx
@@ -1,12 +1,66 @@
-export default function Flashcard() {
-  return (
-    <>
-      <Tile/>
-    </>
-  );
-}
+// export default function Flashcard() {
+//   return (
+//     <>
+//       <Tile/>
+//     </>
+//   );
+// }
 
 
 
 //each tile is a section 
 //
+
+
+
+export default function Tile() {
+
+  const [flashcardBank, setFlashcardBank] = useState ([
+    {
+     question: "What is JSX?", 
+     answer: "JSX stands for JavaScript XML. It allows you to write HTML elements in JavaScript and place them in the DOM without using `createElement()` or `appendChild()`."
+    }, 
+    { 
+     question: "How do you create a React component?", 
+     answer: "You can create a React component either as a function or as a class. Function components are simple JavaScript functions, while class components require extending `React.Component`."
+    }, 
+    {
+     question: "What is the virtual DOM?", 
+     answer: "The virtual DOM is a lightweight copy of the actual DOM. React uses the virtual DOM to determine which elements need to be updated, making updates more efficient."
+    }
+   ])
+
+  return (
+    <>
+
+
+
+
+    </>
+
+  );
+
+}
+
+//create a section 
+//this has styles of clicked and unlciked/ question/answer
+//click on section it will show answer (similar to menu button)
+// const faq = [
+//   {
+//     question: "What is JSX?",
+//     answer:
+//       "JSX stands for JavaScript XML. It allows you to write HTML elements in JavaScript and place them in the DOM without using `createElement()` or `appendChild()`.",
+//   },
+//   {
+//     question: "How do you create a React component?",
+//     answer:
+//       "You can create a React component either as a function or as a class. Function components are simple JavaScript functions, while class components require extending `React.Component`.",
+//   },
+//   {
+//     question: "What is the virtual DOM?",
+//     answer:
+//       "The virtual DOM is a lightweight copy of the actual DOM. React uses the virtual DOM to determine which elements need to be updated, making updates more efficient.",
+//   },
+// ];
+
+// const abcde = faq[0].question;
